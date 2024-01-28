da244fcc00299c4ccf644f838ae55064c5043fac
harui2019 <harui2019@proton.me>
2024-01-28 19:36:26 +0800

    chore: version to v0.6.0

--- version.py
+++ version.py
@@ -1,13 +1,13 @@
-version_main = (0, 5, 0)
-version_dev = ("dev", 1)
-isDev = True
+"""
+
+================================================================
+Capsule Version (:mod:`qurry.capsule.version`)
+================================================================
+"""
+
+version_main = (0, 6, 0)
+"""The main version of Capsule."""
 
 
-def _beta(beta_str: str, serial: int) -> str:
-    return beta_str + str(serial)
-
-
-__version__ = version_main + (_beta(*version_dev),) if isDev else version_main
-__version_str__ = ".".join(map(str, version_main)) + (
-    "." + _beta(*version_dev) if isDev else ""
-)
+__version__ = version_main
+__version_str__ = ".".join(map(str, version_main))
