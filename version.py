aa45a0cd383d3bcd12a1dd8291d9cbb42362f17a
harui2019 <harui2019@proton.me>
2024-03-18 14:10:59 +0800

    chore: version to v0.6.1

--- version.py
+++ version.py
@@ -5,9 +5,8 @@ Capsule Version (:mod:`qurry.capsule.version`)
 ================================================================
 """
 
-version_main = (0, 6, 0)
+version_main = (0, 6, 1)
 """The main version of Capsule."""
 
 
 __version__ = version_main
-__version_str__ = ".".join(map(str, version_main))
